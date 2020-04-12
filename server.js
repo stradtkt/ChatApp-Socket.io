5ccf546b19a7a26d140df6c2969c4b6dfb2a758e
Kevin Stradtman <stradtkt22@gmail.com>
2020-04-12 19:01:51 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+    // console.log('New Connection');
+    // socket.emit('message', 'Welcome to ChatCord!');
+
+    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
